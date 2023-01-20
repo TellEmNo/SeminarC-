b83d68ad7791a1b6cbfa987631f37149a2f4d4aa
Vladislav Verin <dippicaka@gmail.com>
2023-01-20 16:45:48 +0300

    41 is done

---
+++ HomeWork006/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/* 
+void Count(int size){
+	int count = 0;
+	for (int i = 0; i < size; i++){
+		int num = Convert.ToInt32(Console.ReadLine());
+		if(num > 0) count++;
+	}
+	Console.WriteLine();
+	Console.WriteLine($"The number of elements > 0 = {count}");
+}
+
+Console.Write("Input the number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Count(size);
+ */
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
