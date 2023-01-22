6428462333eb805cceb584c098b045dbe200cb9d
Vladislav Verin <dippicaka@gmail.com>
2023-01-22 22:33:28 +0300

    Initial Example 007

--- HomeWork006/Program.cs
+++ HomeWork006/Program.cs
@@ -20,7 +20,8 @@ Count(size);
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных
 // уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
 
-void IntersectionOfLines(double k1, double b1, double k2, double b2){
+void IntersectionOfLines(double k1, double b1, double k2, double b2)
+{
 	double x = (b2 - b1) / (k1 - k2);
 	double y = k1 * x + b1;
 	if (k1 == k2) Console.WriteLine("Решений нет");
