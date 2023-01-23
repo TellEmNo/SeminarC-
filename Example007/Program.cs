7684498829eca8d1ccc3a72404b3de8162177f78
Vladislav Verin <dippicaka@gmail.com>
2023-01-23 16:15:27 +0300

    007 third task is done

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -78,17 +78,8 @@ Show2dArray(secondMatrix);
 // Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
 // (с индексами (0,0); (1; 1) и т.д.
 
-int[,] CreateMatrix()
+int[,] CreateMatrix(int rows, int columns, int minValue, int maxValue)
 {
-	Console.Write("Input a number of rows: ");
-	int rows = Convert.ToInt32(Console.ReadLine());
-	Console.Write("Input a number of columns: ");
-	int columns = Convert.ToInt32(Console.ReadLine());
-	Console.Write("Input a min possible value: ");
-	int minValue = Convert.ToInt32(Console.ReadLine());
-	Console.Write("Input a max possible value: ");
-	int maxValue = Convert.ToInt32(Console.ReadLine());
-
 	int[,] matrix = new int[rows, columns];
 	for (int i = 0; i < matrix.GetLength(0); i++)
 		for (int j = 0; j < matrix.GetLength(1); j++)
@@ -107,18 +98,38 @@ void Show2dArray(int[,] array)
 	Console.WriteLine();
 }
 
-int SumElements(int[,] matrix)
+int SumElements(int[,] matrix, int rows, int columns)
 {
 	int result = 0;
-	for (int i = 0; i < matrix.GetLength(0); i++)
-		result += matrix[i, i];
-	return result;
+	if (rows <= columns)
+	{
+		for (int i = 0; i < matrix.GetLength(0); i++)
+			result += matrix[i, i];
+		return result;
+	}
+	else
+	{
+		for (int i = 0; i < matrix.GetLength(1); i++)
+			result += matrix[i, i];
+		return result;
+	}
 }
 
-int[,] matrix = CreateMatrix();
+Console.Write("Input a number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrix(rows, columns, minValue, maxValue);
 
 Show2dArray(matrix);
 
-int result = SumElements(matrix);
+int result = SumElements(matrix, rows, columns);
 
 Console.WriteLine(result);
+
+// для дз Возврат int и для проверки на существование вывела бы текст что его нет
